01483f22dc371486f495b08da7cc7e390ab7c5ea
ifuller140 <ianfuller140@gmail.com>
2026-04-13 16:21:44 -0400

    Haardcoded grab and swap

---
+++ Lab2/manipulator_grabandswap.py
@@ -0,0 +1,103 @@
+import robomaster
+from robomaster import robot
+import time
+
+def sub_data_handler(sub_info):
+    pos_x, pos_y = sub_info
+
+    # Fix the integer overflow in pos_y
+    if pos_y > 2**31 - 1:
+        pos_y = pos_y - 2**32
+
+    # You can use these values to confirm the robot arm is where it is supposed to be
+    # It is also usable for determine the right setpoints to send to "moveto" commands
+    print("Robotic Arm: pos x:{0}, pos y:{1}".format(pos_x, pos_y))
+
+if __name__ == '__main__':
+    robomaster.config.ROBOT_IP_STRING = "192.168.50.116"
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="sta", sn="3JKCH8800100VW")
+    ep_arm = ep_robot.robotic_arm
+    ep_chassis = ep_robot.chassis
+    ep_gripper = ep_robot.gripper
+
+    # Start printing the gripper position
+    ep_arm.sub_position(freq=5, callback=sub_data_handler)
+
+    # Open the gripper
+    #ep_gripper.open(power=50)
+    #time.sleep(1)
+    #ep_gripper.pause()
+
+    
+
+    # Move the arm to the "retracted" position
+    ep_robot.robotic_arm.moveto(x=185, y=-80).wait_for_completed()
+
+    # Close the gripper
+    ep_gripper.close(power=50)
+    time.sleep(1)
+    ep_gripper.pause()
+    
+    # Move forward
+    ep_chassis.move(x=0.1, y=0, z=0, xy_speed=0.7).wait_for_completed()
+
+    # Move to the up position
+    ep_robot.robotic_arm.moveto(x=175, y=140).wait_for_completed()
+    time.sleep(1)
+
+    # Move forward
+    ep_chassis.move(x=0.1, y=0, z=0, xy_speed=0.7).wait_for_completed()
+    time.sleep(1)
+    ep_chassis.move(x=0.1, y=0, z=0, xy_speed=0.7).wait_for_completed()
+    time.sleep(1)
+
+    # Move to the down position
+    ep_robot.robotic_arm.moveto(x=175, y=80).wait_for_completed()
+    time.sleep(1)
+
+    # Move backward
+    ep_chassis.move(x=-0.1, y=0, z=0, xy_speed=0.7).wait_for_completed()
+
+    # Open the gripper
+    ep_gripper.open(power=60)
+    time.sleep(1)
+    ep_gripper.pause()
+
+
+    # Move backward
+    ep_chassis.move(x=-0.1, y=0, z=0, xy_speed=0.7).wait_for_completed()
+    ep_chassis.move(x=-0.1, y=0, z=0, xy_speed=0.7).wait_for_completed()
+
+    # Move the arm to the "retracted" position
+    ep_robot.robotic_arm.moveto(x=185, y=-80).wait_for_completed()
+
+    # Close the gripper
+    ep_gripper.close(power=50)
+    time.sleep(1)
+    ep_gripper.pause()
+
+    # Move to the down position
+    ep_robot.robotic_arm.moveto(x=175, y=-50).wait_for_completed()
+    time.sleep(1)
+
+    # Move the arm forward and down in order to pickup an object
+    # (we do this in two moves to avoid the "keep out zone" where the robot may hit itself)
+    #ep_robot.robotic_arm.moveto(x=180, y=30).wait_for_completed()
+    #ep_robot.robotic_arm.moveto(x=180, y=-50).wait_for_completed()
+
+    # Close the gripper on the object
+    #ep_gripper.close(power=50)
+    #time.sleep(1)
+    #ep_gripper.pause()
+
+    # Lift the object in the gripper
+    #ep_robot.robotic_arm.moveto(x=180, y=100).wait_for_completed()
+
+    # Relative move commands work inconsistently but if you want to use them they are:
+    # ep_arm.move(x=50).wait_for_completed()
+    # ep_arm.move(x=-50).wait_for_completed()
+    # ep_arm.move(y=50).wait_for_completed()
+    # ep_arm.move(y=-50).wait_for_completed()
+
+    ep_robot.close()
